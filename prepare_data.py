352d8a94ae63f3746c9a858eb57b20b9e0d177c9
Huahuan Zheng <maxwellzh@outlook.com>
2022-03-10 20:21:08 +0800

    fix data preparation bug due to torchvision update

--- prepare_data.py
+++ prepare_data.py
@@ -1,45 +1,24 @@
-import torch
-import numpy as np
-from PIL import Image
+
 import os
-from torchvision import datasets, transforms
+# tested with
+# torch==1.10.2
+# torchvision==0.11.3
+from torchvision import datasets
 
-datasets.MNIST('./data', train=True, download=True)
-
-source_dir = 'data/MNIST/processed/'
 target_dir = 'data/MNIST'
+os.makedirs(target_dir, exist_ok=True)
 
-training_images = torch.load(source_dir + '/training.pt')
-test_images = torch.load(source_dir + '/test.pt')
-
-# train set
-traindir = target_dir + '/train/'
-os.makedirs(traindir, exist_ok=True)
-images = training_images[0].numpy()
-labels = training_images[1].numpy()
-print("Images shape:", images.shape)
-print("Label shape:", labels.shape)
-size = labels.shape[0]
-for i in range(size):
-    l = labels[i].item()
-    os.makedirs(traindir + str(l), exist_ok=True)
-    img = Image.fromarray(images[i])
-    img.save(traindir + '{}/{}_{:05}.bmp'.format(l, l, i))
-    print('\rProcessed: [{}/60000]'.format(i+1), end='')
-print('')
-
-# test set
-testdir = target_dir + '/test/'
-os.makedirs(traindir, exist_ok=True)
-images = test_images[0].numpy()
-labels = test_images[1].numpy()
-
-size = labels.shape[0]
-for i in range(size):
-    l = labels[i].item()
-    os.makedirs(testdir + str(l), exist_ok=True)
-    img = Image.fromarray(images[i])
-    img.save(testdir + '{}/{}_{:05}.bmp'.format(l, l, i))
-    print('\rrProcessed: [{}/10000]'.format(i+1), end='')
-
-print('')
+# images will be stored at data/train and data/test
+for subset in ['train', 'test']:
+    print(f"Processing {subset} set:")
+    train_images = datasets.MNIST(
+        './data', train=(subset == 'train'), download=True)
+    traindir = os.path.join(target_dir, subset)
+    os.makedirs(traindir, exist_ok=True)
+    size = len(train_images)
+    for i in range(size):
+        img, label = train_images[i]
+        os.makedirs(os.path.join(traindir, str(label)), exist_ok=True)
+        img.save(os.path.join(traindir, '{0}/{0}_{1:05}.bmp'.format(label, i)))
+        print('\rProcessing: [{}/{}]'.format(i+1, size), end='')
+    print('')
